f7c6ccd11ce9294f53714bc72a3b60a80e8179cb
Hannes Weisbach <hannes.weisbach@gmail.com>
2019-08-19 13:41:55 +0200

    Update extract_fg_bw and document it in README

--- postproc/README.md
+++ postproc/README.md
@@ -17,7 +17,21 @@ However, using those scripts is not strictly necessary, since we report simple
 quantiles (minimum, median, 95th, 99th, 99.9th, and maximum) which can be
 extracted using different tooling as well.
 
-## Step 1 - Filtering and extracting data
+## Step 1 - Extracting Data Rates for Foreground and Background loads
+
+The script ```extract_fg_bw.rb``` is a Ruby script, that extracts for each
+benchmark configuration the target background data rate, achieved data rate
+(total, read, and write), total data rate of background workloads, and
+foreground workloads (total, read, and write).
+
+The data is used for the x-axis labels and achieved data rate is represented as
+colour in the plots.
+
+The data is written to a file ```fg_bw.dat```, for each benchmark
+configuration, in its corresponding sub-directory in the
+```generated/data```-directory.
+
+## Step 2 - Filtering and extracting data
 
 The ```postproc_k2.R``` file contains an R script doing all the heavy lifting.
 It consumes all results from all benchmark configuration and extracts the
@@ -29,7 +43,7 @@ and maximum latency tables. Output files are:
 ```generated/data/mb```  
 ```generated/data/ml```
 
-## Step 2 - Generating TeX files
+## Step 3 - Generating TeX files
 
 The script ```table_data_to_tex.rb``` consumes the following files:
 
--- postproc/scripts/extract_fg_bw.rb
+++ postproc/scripts/extract_fg_bw.rb
@@ -4,42 +4,75 @@ require 'pathname'
 
 avg = Regexp.new(/avg=(\d+.\d+)/)
 
-Dir.glob("k2-results/**/").each do |dir|
+raw_data_dir = Pathname.new("k2-results")
+output_dir = Pathname.new("generated").join("data")
+
+Dir.glob("#{raw_data_dir}/**/").each do |dir|
   # meh. I want increasing BW :/
   data = []
   Dir.glob(File.join(dir, "*.out")). each do |file|
-    p file
-    # Wind rushing through your hair, skating by the edge … fuck error checking
+    print "Proccesing #{file}\n"
+    # Extract target bandwidth from filename
     bw = /K(\d+)\.out/.match(file)[1].to_i
-    h = { "bw" =>  bw, "r" => 0, "w" => 0}
-    ["realtime", "realtime2"].each do |rt|
-      read = `tail -n +6 #{file} | grep -A 17 #{rt}: | grep read: -A 7 | grep avg`
-      write = `tail -n +6 #{file} | grep -A 17 #{rt}: | grep write: -A 7 | grep avg`
-      match_read = avg.match(read)
-      match_write = avg.match(write)
 
-    #p match_read, match_write
+    # TODO: this is awfully slow, because it file is read multiple times and
+    # several new processes are spawned.
+
+    # extract total read and write data rates
+    # is automatically 0.0 if no READ or WRITE line is found in "file".
+    read = `awk \' /READ:/ { sub("MiB/s", "", $2); print substr($2, 4); }\' #{file}`.to_f
+    write = `awk \' /WRITE:/ { sub("MiB/s", "", $2); print substr($2, 4); }\' #{file}`.to_f
+
+    h = {
+      "bw" => bw,              # target data rate
+      "total" => read + write, # total accumulated data rate
+      "r" => read,             # accumulated read data rate
+      "w" => write,            # accumulated write data rate
+      "rt_r" => 0,             # accumulated read data rate of RT procs
+      "rt_w" => 0              # accumulated write data rate of RT procs
+    }
+
+    # for up to two realtime processes
+    ["realtime", "realtime2"].each do |rt|
+      # find read and write bandwidth
+      rt_read = `tail -n +6 #{file} | grep -A 17 #{rt}: | grep read: -A 7 | grep avg`
+      rt_write = `tail -n +6 #{file} | grep -A 17 #{rt}: | grep write: -A 7 | grep avg`
+      match_read = avg.match(rt_read)
+      match_write = avg.match(rt_write)
+
+      # convert to MiB/s
       if match_read
-        h["r"] += match_read[1].to_f / 1024.0
+        h["rt_r"] += match_read[1].to_f / 1024.0
       end
       if match_write
-        h["w"] += match_write[1].to_f / 1024.0
+        h["rt_w"] += match_write[1].to_f / 1024.0
       end
     end
 
-    if h["r"] == 0 and h["w"] == 0
+    # we should have either written or read something.
+    if h["r"] == 0 and h["w"] == 0 or h["rt_r"] == 0 and h["rt_w"] == 0
       raise "#{file} has both r and w 0"
     end
 
+    # collect extracted data
     data << h
   end
 
+  # if we have extracted any data (empty file?)
   if not data.empty?
-    outfile = File.join(dir, "fg_bw.dat")
+    # write to "generated" directory, but keep the rest of the hierarchy
+    outfile = output_dir + Pathname.new(dir).relative_path_from(raw_data_dir) + "fg_bw.dat"
+    # create dir, if not exists
+    outfile.dirname.mkpath unless outfile.dirname.exist?
+    # create + write the file.
     File.open(outfile, "w") do |file|
-      file.write("bw fg_total fg_r fg_w\n")
+      # write total bandwidth, foreground + background bandwidth,
+      # foreground read and foreground write bandwidth out to file
+      file.write("bw total r w bg_total fg_total fg_r fg_w\n")
       data.sort_by {|e| e["bw"]}.each do |line|
-        file.write("#{line["bw"]} #{line["r"] + line["w"]} #{line["r"]} #{line["w"]}\n")
+        file.write("#{line["bw"]} #{line["total"]} #{line["r"]} #{line["w"]} ")
+        file.write("#{line["total"] - line["rt_r"] - line["rt_w"]} ")
+        file.write("#{line["r"] + line["w"]} #{line["r"]} #{line["w"]}\n")
       end
     end
   end
